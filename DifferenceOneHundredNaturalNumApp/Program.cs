c8403266421996d44de520e879e48416ffe2f376
zihadul <zihadulislam41@gmail.com>
2015-05-13 16:04:48 +0600

    initial file

---
+++ DifferenceOneHundredNaturalNumApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferenceOneHundredNaturalNumApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numA = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                numA.Add(i*i);
+            }
+          
+            int sumOfSq = numA.Sum();
+            
+
+            List<int> numB = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                numB.Add(i);
+            }
+            int sqSum = (numB.Sum()) * (numB.Sum());
+            
+
+            Console.WriteLine(sqSum-sumOfSq);
+            Console.ReadKey();
+        }
+    }
+}
